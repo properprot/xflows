c8bb9edad995f946d64f1bd20808625188bdc285
PolyAnthi <properprotgh@thepoly.eu>
2025-09-27 21:16:04 +0000

    config: create base configuration setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/properprot/xflows
 
 go 1.22.2
+
+require gopkg.in/yaml.v2 v2.4.0
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,5 +1,41 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
 type Config struct {
-	LogLevel string `yaml:"log_level"`
+	LogLevel string         `yaml:"log_level"`
+	Outputs  []OutputConfig `yaml:"outputs"`
+	SFlow    SFlowConfig    `yaml:"sflow"`
+}
+
+type OutputConfig struct {
+	Type   string                 `yaml:"type"`
+	Config map[string]interface{} `yaml:"config"`
+}
+
+type SFlowConfig struct {
+	AgentIP string `yaml:"agent_ip"`
+	// We don't grab the rate from XDP itself so it needs to be from config (for multipliers)
+	SampleRate uint32 `yaml:"sample_rate"`
+	// Incase the user wants to sep out as needed (if collector primarily)
+	Version uint32 `yaml:"version"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read config file: %w", err)
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed to parse config file: %w", err)
+	}
+
+	return &config, nil
 }
